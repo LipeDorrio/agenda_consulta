84fbd77a366319ce82867def64c7eb7b2a493013
LipeDorrio <felipejohnjohn@gmail.com>
2025-08-01 18:33:50 -0300

    feat: add onClick handler to Button component for improved interactivity

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,13 @@ import type { ReactNode } from "react";
 type Props = {
   children: ReactNode;
   classe?: string;
+  onClick?: () => void;
 };
 
-function Button({children, classe}: Props) {
+function Button({children, classe, onClick}: Props) {
   const padraoClasse = "font-semibold flex gap-3 justify-center outline-blue-400 hover:outline-1 transform duration-200 hover:scale-101 hover:shadow-xl rounded-2xl p-2 text-center"
   return (
-    <button className={`${padraoClasse} ${classe}`}>
+    <button className={`${padraoClasse} ${classe}`} onClick={onClick}>
       {children}
     </button>
   );
