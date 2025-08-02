276ab355d89a0e49c5ae0a19b0302d02b665aa59
LipeDorrio <felipejohnjohn@gmail.com>
2025-08-02 02:48:30 -0300

    feat: Retirando a filtragem de App.tsx e adicionando um modal para cadastro ou edição

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import Input from "./components/Input";
 import { useState } from "react";
 import Modal from "./components/Modal";
+import Filters from "./components/Filters";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -12,50 +13,21 @@ function App() {
     <div className="h-screen w-screen flex flex-col border-1">
       <div className="m-4">
         <Navbar />
-        <div className="border-1 p-5 w-full rounded-3xl bg-zinc-50 shadow-xl">
-          <h1 className="font-bold text-2xl flex gap-3">
-            <Funnel />
-            Filtros
-          </h1>
-          <div className="grid grid-cols-4 gap-3 p-3 rounded-2xl my-3">
-            <select
-              className="outline-1 hover:outline-blue-900 rounded-2xl p-3"
-              name=""
-              id=""
-            >
-              <option value="">Option 1</option>
-              <option value="" disabled selected>
-                Selecione um valor
-              </option>
-            </select>
-            <select
-              className="outline-1 hover:outline-blue-900 rounded-2xl p-3"
-              name=""
-              id=""
-            >
-              <option value="" disabled selected>
-                Selecione um valor
-              </option>
-              <option value="teste" selected>
-                teste
-              </option>
-              <option value="">Option 2</option>
-            </select>
-            <Input type="date" />
-            <Button classe="bg-zinc-100">
-              <FunnelX />
-              Limpar Filtros
-            </Button>
-          </div>
-        </div>
+        <Filters />
       </div>
       <button
-        onClick={() => {setIsModalOpen(true)}}
-        className="w-15 h-15 bg-zinc-500 text-white p-3 rounded-full fixed bottom-7 right-7 shadow-xl hover:bg-zinc-600 transition duration-300 hover:scale-110"
+        onClick={() => {
+          setIsModalOpen(true);
+        }}
+        className="w-15 h-15 bg-green-500 text-white p-3 rounded-full fixed bottom-7 right-7 shadow-xl hover:bg-green-600 transition duration-300 hover:scale-110"
       >
         <Plus className="flex items-center justify-center w-full h-full" />
       </button>
-      <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+      <Modal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        title="Cadastro"
+      />
     </div>
   );
 }
