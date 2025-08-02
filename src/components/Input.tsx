331e99bdabff2263c6e77d363b55faaf305e34b8
LipeDorrio <felipejohnjohn@gmail.com>
2025-08-02 02:49:24 -0300

    feat:  Adicionando label para os inputs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,11 +2,28 @@ type Props = {
   classe?: string;
   type?: string;
   placeholder?: string;
+  label?: string;
+  nm_label?: string;
 };
 
-function Input({ classe, type, placeholder }: Props) {
+function Input({ classe, type, placeholder, label, nm_label }: Props) {
   const padraoClasse = "outline-1 hover:outline-blue-900 rounded-2xl p-3";
-  return <input className={`${padraoClasse} ${classe}`} type={type} placeholder={placeholder} />;
+
+  return (
+    <div className="flex flex-col">
+      {label && (
+        <label htmlFor={nm_label} className="mb-2">
+          {label}
+        </label>
+      )}
+      <input
+        className={`${padraoClasse} ${classe}`}
+        type={type}
+        placeholder={placeholder}
+        id={nm_label}
+      />
+    </div>
+  );
 }
 
 export default Input;
