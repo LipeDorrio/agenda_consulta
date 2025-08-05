2e97deb58753cfc5eb2d1d895884de3980d5944b
LipeDorrio <felipejohnjohn@gmail.com>
2025-08-05 01:30:35 -0300

    refactor: Simplify App component by removing unused imports and modal functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
-import { Funnel, FunnelX, Plus } from "lucide-react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Button from "./components/Button";
-import Input from "./components/Input";
-import { useState } from "react";
-import Modal from "./components/Modal";
-import Filters from "./components/Filters";
+import Agenda from "./pages/Agenda";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
-    <div className="h-screen w-screen flex flex-col border-1">
-      <div className="m-4">
-        <Navbar />
-        <Filters />
-      </div>
-      <button
-        onClick={() => {
-          setIsModalOpen(true);
-        }}
-        className="w-15 h-15 bg-green-500 text-white p-3 rounded-full fixed bottom-7 right-7 shadow-xl hover:bg-green-600 transition duration-300 hover:scale-110"
-      >
-        <Plus className="flex items-center justify-center w-full h-full" />
-      </button>
-      <Modal
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-        title="Cadastro"
-      />
+    <div className="h-screen w-screen flex flex-col ">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Agenda />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
