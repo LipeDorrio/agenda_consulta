cf34059ebbe9628bfdfed3c0551426b2b3acaade
LipeDorrio <felipejohnjohn@gmail.com>
2025-08-05 01:30:56 -0300

    feat: Implement Dashboard component for displaying statistics and layout

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,20 @@
+function Dashboard() {
+  return (
+    <div className="m-5 grid grid-cols-3 h-full gap-5">
+      <div className="h-full border-1 shadow-2xl bg-zinc-100 flex justify-center rounded-xl items-center">
+        Dashboard
+      </div>
+      <div className="h-full border-1 shadow-2xl bg-zinc-100 flex justify-center rounded-xl items-center">
+        Dashboard
+      </div>
+      <div className="h-full border-1 shadow-2xl bg-zinc-100 flex justify-center rounded-xl items-center">
+        Dashboard
+      </div>
+      <div className="h-full border-1 shadow-2xl bg-zinc-100 flex justify-center rounded-xl col-span-3 items-center">
+        Estatísticas
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
